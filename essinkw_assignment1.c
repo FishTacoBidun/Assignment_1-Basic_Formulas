126205d48cf314f7598b0856727bdba92b98612f
FishTacoBidun <wolfie.essink@gmail.com>
2025-06-26 17:49:35 -0700

    Update essinkw_assignment1.c

--- essinkw_assignment1.c
+++ essinkw_assignment1.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 #include <math.h>
+
+int getNumSpheres(void);
+void getSphereInfo(int numSpheres);
+void findSphereSA(double R, double ha, double hb);
+void findSphereV(double R, double ha, double hb);
+
+int main(void) {
+
+  
+
+return 0;
+}
+
+//gets the number of spheres the user wants to perform calculations on
+int getNumSpheres(void)
+{
+
+}
+
+//gets the dimensions of a sphere needed to perform calculations
+void getSphereInfo(int numSpheres)
+{
+
+}
+
+//does the math to find the sphere's surface area 
+void findSphereSA(double R, double ha, double hb)
+{
+
+}
+
+//does the math to find the sphere's volume area 
+void findSphereV(double R, double ha, double hb)
+{
+
+}
